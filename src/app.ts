27ddd28573343594a1aeea62146241a756e7b922
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-11-15 12:13:54 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,61 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { Schema } from "mongoose";
+const app: Application = express();
+
+// using cors
+app.use(cors());
+
+// parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  // res.send(`Server is running`);
+  /*
+  step 1 : Interface
+  step 2 : Schema
+  step 3: Model
+  step 4 : Database Query 
+  */
+
+  // crating interface
+  interface IUser {
+    id: string;
+    role: "student";
+    password: string;
+    name: {
+      firstName: string;
+      middleName: string;
+      lastName: string;
+    };
+    dateOfBirth?: string;
+    gender: "male" | "female";
+    email?: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    presentAddress: string;
+    permanentAddress: string;
+  }
+
+  // creating schema
+  const userSchema = new Schema<IUser>({
+    id: { type: String, required: true, unique: true },
+    role: { type: String, required: true },
+    password: { type: String, required: true },
+    name: {
+      firstName: { type: String, required: true },
+      middleName: { type: String },
+      lastName: { type: String, required: true },
+    },
+    dateOfBirth: { type: String, required: true },
+    gender: { type: String, enum: ["Male", "Female"], required: true },
+    email: { type: String, required: true },
+    contactNo: { type: String, required: true },
+    emergencyContactNo: { type: String, required: true },
+    presentAddress: { type: String, required: true },
+    permanentAddress: { type: String, required: true },
+  });
+});
+
+export default app;
